a7fd0acadf317adc8b75db4446c17e17b0b39660
Burak <burakkahyabk@gmail.com>
2024-06-02 22:40:57 +0300

    Added NoProjectSelected component for default look

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import NewProject from './components/NewProject';
+import NoProjectsSelected from './components/NoProjectsSelected';
 import ProjectsSidebar from './components/ProjectsSideBar';
+
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectsSelected />
     </main>
   );
 }
